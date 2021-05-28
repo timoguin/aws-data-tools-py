844e0a04be740ad1038d5b5b196f9ee955694441
Tim O'Guin <tim.oguin@cbsinteractive.com>
2021-05-27 21:20:01 -0500

    add initial (untested) json schemas for all the organization types, re-org library structure

--- aws_data_tools/utils/__init__.py
+++ aws_data_tools/utils/__init__.py
@@ -5,7 +5,7 @@ from humps import depascalize, pascalize
 DEFAULT_PAGINATION_LIMIT = 500
 
 
-def api(service: str, **kwargs: dict[str, str]):
+def api(service: str, data_key: str = None, **kwargs):
     """
     Wrapper for a boto3 client that uses the humps library to automatically
     convert API keys from pascal case to snake cake
@@ -15,11 +15,14 @@ def api(service: str, **kwargs: dict[str, str]):
     kwargs = pascalize(kwargs)
     response = getattr(client, func)(**kwargs)
     depascalized_response = depascalize(response)
+    if data_key is not None:
+        return depascalized_response.get('data_key')
     return depascalized_response
 
 
 def paged_api(service: str,
-              key: str,
+              data_key: str = None,
+              data_index: int = None,
               page_limit: int = DEFAULT_PAGINATION_LIMIT,
               **kwargs):
     """
@@ -37,8 +40,31 @@ def paged_api(service: str,
     depascalized_responses = []
     for page in page_iterator:
         depascalized_page = depascalize(page).get(key)
+        if data_key is not None:
+            depascalized_page = depascalize(page).get(key)
+        else:
+            depascalized_page = depascalize(page)
         depascalized_responses.extend(depascalized_page)
+    if data_index is not None:
+        return depascalized_responses[0]
     return depascalized_responses
+
+
+@dataclass
+class OrganizationSession:
+    session: botocore.session.Session
+    client: boto3.client
+    paginator: botocore.paginate.Paginator
+    pagination_config: Dict[str, int] = field(default={'MaxItems': DEFAULT_PAGINATION_LIMIT})
+
+    def __get_page_iterator
+
+    def __init__(self, pagination_config: Dict[str, int] = None, **kwargs):
+        self.session = botocore.session.Session()
+        self.client = self.session.client('organizations')
+        self.paginator = self.client.get_paginator()
+        if pagination_config is not None:
+            self.pagination_config = pagination_config
 
 
 def ugly_api(service: str, **kwargs: dict[str, str]):
