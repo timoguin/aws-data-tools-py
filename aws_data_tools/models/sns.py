4e55f2b18ebe34a999b69dfecede6b29fbacb678
Tim O'Guin <timoguin@gmail.com>
2021-09-09 17:19:53 -0500

    Revert changes to models.SnsMessage, add new LambdaSnsMessage model

--- aws_data_tools/models/sns.py
+++ aws_data_tools/models/sns.py
@@ -30,12 +30,31 @@ class SnsMessageData(ModelBase):
 class SnsMessage(ModelBase):
     """Schema for an SNS message"""
 
-    event_source: str
-    event_version: str
-    event_subscription_arn: str
-    sns: SnsMessageData
+    message: str
+    message_id: str
+    signature: str
+    signature_version: str
+    signing_cert_url: str
+    subject: str
+    timestamp: str
+    topic_arn: str
+    type: str
+    unsubscribe_url: str
+
+    # TODO: Unsure if there are additional attributes if the message comes from an SNS
+    # FIFO topic
 
     @property
     def is_body_json(self) -> bool:
         """Check if the message body is a JSON string"""
-        return is_valid_json(self.sns.message)
+        return is_valid_json(self.message)
+
+
+@dataclass
+class LambdaSnsMessage(ModelBase):
+    """Represents the SNS message format when using Lambda subscriptions"""
+
+    event_source: str
+    event_version: str
+    event_subscription_arn: str
+    sns: SnsMessage
